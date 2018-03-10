6fa7bb71d959d9145432470fb0c4e48a43836d02
Paul Aswa <aswapaul@gmail.com>
2018-03-10 22:00:05 +0300

    Set up PyPI description
    Add requirement pypandoc

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
+import pypandoc
 from setuptools import setup
 
+long_description = pypandoc.convert_file('README.md', 'rst')
+
 setup(name='pyconfigreader',
-      version='0.2.1',
+      version='0.2.3',
       description='A simple module for handling configurations and config files',
+      long_description=long_description,
       url='http://github.com/giantas/pyconfigreader',
       author='Aswa Paul',
       license='MIT',
