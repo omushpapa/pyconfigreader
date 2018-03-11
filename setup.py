f2501f017e58d2479887ff906205f5c820aec2a8
Paul Aswa <aswapaul@gmail.com>
2018-03-11 22:38:21 +0300

    Fix: pypandoc import

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-import pypandoc
 from setuptools import setup
 
 try:
+    import pypandoc
     long_description = pypandoc.convert_file('README.md', 'rst')
 except ImportError:
     long_description = 'Usage available at http://github.com/giantas/pyconfigreade'
 
 setup(name='pyconfigreader',
-      version='0.2.4',
+      version='0.2.5',
       description='A simple module for handling configurations and config files',
       long_description=long_description,
       url='http://github.com/giantas/pyconfigreader',
