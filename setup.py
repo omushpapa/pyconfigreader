2efc288a830ff965c35ed63c1d8d0343dc86cd28
Aswa Paul <aswapaul@gmail.com>
2020-08-09 21:20:46 +0300

    Bump version: 0.7.1 → 0.8.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except (ImportError, OSError, RuntimeError):
     long_description = 'Usage available at http://github.com/giantas/pyconfigreader'
 
 setup(name='pyconfigreader',
-      version='0.7.1',
+      version='0.8.0',
       description='A module for handling simple configuration requirements',
       long_description=long_description,
       url='http://github.com/giantas/pyconfigreader',
