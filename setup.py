64f24062ccf567688dd016bfe264c5bb903db77e
Aswa Paul <aswapaul@gmail.com>
2020-08-11 09:17:16 +0300

    Remove testfixtures from abstract dependencies

--- setup.py
+++ setup.py
@@ -14,9 +14,7 @@ setup(name='pyconfigreader',
       author='Aswa Paul',
       license='MIT',
       packages=['pyconfigreader'],
-      install_requires=[
-          'testfixtures',
-      ],
+      install_requires=[],
       zip_safe=False,
       python_requires='>3.4, <3.8',
       classifiers=[
