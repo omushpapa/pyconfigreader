d9dba3a1a744e11af9d453318bf0740b87515e24
Aswa Paul <aswapaul@gmail.com>
2022-04-07 17:49:58 +0300

    Feat/py3.8 (#12)
    * Bump py from 1.9.0 to 1.10.0
    
    Bumps [py](https://github.com/pytest-dev/py) from 1.9.0 to 1.10.0.
    - [Release notes](https://github.com/pytest-dev/py/releases)
    - [Changelog](https://github.com/pytest-dev/py/blob/master/CHANGELOG.rst)
    - [Commits](https://github.com/pytest-dev/py/compare/1.9.0...1.10.0)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump urllib3 from 1.25.10 to 1.26.5
    
    Bumps [urllib3](https://github.com/urllib3/urllib3) from 1.25.10 to 1.26.5.
    - [Release notes](https://github.com/urllib3/urllib3/releases)
    - [Changelog](https://github.com/urllib3/urllib3/blob/main/CHANGES.rst)
    - [Commits](https://github.com/urllib3/urllib3/compare/1.25.10...1.26.5)
    
    ---
    updated-dependencies:
    - dependency-name: urllib3
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Add support for python 3.8
    
    * Update lock file
    
    * Bump version: 0.8.2 → 0.9.0
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except (ImportError, OSError, RuntimeError):
     long_description = 'Usage available at http://github.com/giantas/pyconfigreader'
 
 setup(name='pyconfigreader',
-      version='0.8.2',
+      version='0.9.0',
       description='A module for handling simple configuration requirements',
       long_description=long_description,
       url='http://github.com/giantas/pyconfigreader',
@@ -16,12 +16,13 @@ setup(name='pyconfigreader',
       packages=['pyconfigreader'],
       install_requires=[],
       zip_safe=False,
-      python_requires='>3.4, <3.8',
+      python_requires='>3.4, <3.9',
       classifiers=[
           "Programming Language :: Python",
           "Programming Language :: Python :: 3",
           "Programming Language :: Python :: 3.5",
           "Programming Language :: Python :: 3.6",
           "Programming Language :: Python :: 3.7",
+          "Programming Language :: Python :: 3.8",
       ]
 )
