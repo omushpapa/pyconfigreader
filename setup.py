d35a7ede30113adde8b01f1ae242ddd822d61eca
Paul Aswa <aswapaul@gmail.com>
2022-04-07 17:36:16 +0300

    Add support for python 3.8

--- setup.py
+++ setup.py
@@ -16,12 +16,13 @@ setup(name='pyconfigreader',
       packages=['pyconfigreader'],
       install_requires=[],
       zip_safe=False,
-      python_requires='>3.4, <3.8',
+      python_requires='>3.4, <3.9',
       classifiers=[
           "Programming Language :: Python",
           "Programming Language :: Python :: 3",
           "Programming Language :: Python :: 3.5",
           "Programming Language :: Python :: 3.6",
           "Programming Language :: Python :: 3.7",
+          "Programming Language :: Python :: 3.8",
       ]
 )
